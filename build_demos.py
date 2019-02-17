f65d17b4336f8ab7c8ba7e95ac071f6cebb6a178
Brock Fanning <brockfanning@gmail.com>
2019-02-17 11:02:44 -0500

    Use existing project for this

--- build_demos.py
+++ build_demos.py
@@ -38,7 +38,7 @@ for abbrev in demos:
     with open(config_file, 'r') as stream:
         yamldata = (yaml.load(stream))
     # Set a new baseurl to match this project's name.
-    yamldata['baseurl'] = '/open-sdg-demos/' + abbrev
+    yamldata['baseurl'] = '/sdg-translation-demo/' + abbrev
     # Point to the latest sdg-translations data.
     for item in yamldata['jekyll_get_data']:
         if item['data'] == 'translations':
