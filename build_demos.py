180beabdef030d164682b795294c1763490b9679
Brock Fanning <brockfanning@gmail.com>
2019-02-17 10:44:46 -0500

    Script progress

--- build_demos.py
+++ build_demos.py
@@ -1,5 +1,7 @@
 """
 Build multiple demos of Open SDG implementations, using all available languages.
+
+After running this script, the demos should be available in a 'builds' folder.
 """
 
 import fileinput
@@ -7,6 +9,7 @@ import os
 import shutil
 import glob
 import sys
+import subprocess
 import yaml
 from git import Repo
 
@@ -26,6 +29,8 @@ demos = {
     'uk': 'https://github.com/ONSDigital/sdg-indicators',
 }
 
+os.mkdir('builds')
+
 for abbrev in demos:
     # Clone the site repository.
     site_folder = os.path.join('repos', abbrev)
@@ -34,8 +39,8 @@ for abbrev in demos:
     config_file = os.path.join(site_folder, '_config.yml')
     with open(config_file, 'r') as stream:
         yamldata = (yaml.load(stream))
-    # Set a new baseurl.
-    yamldata['baseurl'] = '/demos/' + abbrev
+    # Set a new baseurl to match this project's name.
+    yamldata['baseurl'] = '/open-sdg-demos/' + abbrev
     # Point to the latest sdg-translations data.
     for item in yamldata['jekyll_get_data']:
         if item['data'] == 'translations':
@@ -48,15 +53,16 @@ for abbrev in demos:
             continue
         # We only care about these three Jekyll folders.
         for jekyll_folder in ['_indicators', '_goals', '_pages']:
-            new_folder = os.path.join(site_folder, jekyll_folder, language)
+            old_folder = os.path.join(site_folder, jekyll_folder)
+            new_folder = os.path.join(old_folder, language)
             # Abort if folder already exists.
             if os.path.isdir(new_folder):
                 continue
             # Create the folder.
             os.mkdir(new_folder)
             # Copy all files (but no folders) into the new folder.
-            for filename in os.listdir(folder):
-                source_file = os.path.join(site_folder, jekyll_folder, filename)
+            for filename in os.listdir(old_folder):
+                source_file = os.path.join(old_folder, filename)
                 new_file = os.path.join(new_folder, filename)
                 if os.path.isdir(source_file):
                     continue
@@ -64,11 +70,11 @@ for abbrev in demos:
             # Change the copied files to reflect the new language.
             for line in fileinput.input(glob.glob(new_folder + '/*'), inplace=True):
                 # Skip a few unnecessary lines.
-                if folder == '_goals' and line.startswith('title:'):
+                if jekyll_folder == '_goals' and line.startswith('title:'):
                     continue
-                if folder == '_goals' and line.startswith('short:'):
+                if jekyll_folder == '_goals' and line.startswith('short:'):
                     continue
-                if folder == '_goals' and line.startswith(' '):
+                if jekyll_folder == '_goals' and line.startswith(' '):
                     continue
                 # Look for the permalink line, and add the language.
                 if line.startswith('permalink: /'):
@@ -76,3 +82,11 @@ for abbrev in demos:
                     sys.stdout.write('language: ' + language + '\n')
                 else:
                     sys.stdout.write(line)
+
+    # Note: I don't think the following will work in non-Unix environments.
+
+    # Now build the site.
+    subprocess.call('bundle install', cwd=site_folder, shell=True)
+    subprocess.call('bundle exec jekyll build', cwd=site_folder, shell=True)
+    # And move the build into a 'builds' folder.
+    subprocess.call('mv _site ../../builds/' + abbrev, cwd=site_folder, shell=True)
